ee89d6096b507343cffd7ca6d5f0754a5b37ab58
novas <zhuzibaijiamojia@163.com>
2016-04-04 01:14:44 +0800

    增加日期

--- src/com/novas/chart/DataAccess.java
+++ src/com/novas/chart/DataAccess.java
@@ -6,8 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Created by novas on 2016/3/23.
@@ -78,11 +77,71 @@ System.out.println("init");
 
         System.out.println("in do get");
         String dataselect=req.getParameter("dataselect");
-        System.out.println("dataselect="+dataselect);
+        String type=req.getParameter("type");
+        String[] args=map.get(dataselect);
         resp.setContentType("text/html;charset=GBK");
         PrintWriter out = resp.getWriter();
-        out.print(dataselect);
-        out.close();
+        if(type.equals("year"))
+        {
+            ArrayList<String> yearlist=new ArrayList<String>();
+            for(int i=0;i<args.length;i++)
+            {
+                String[] temps=args[i].split("/");
+                String temp=temps[0];
+                if(!yearlist.contains(temp))
+                yearlist.add(temp);
+            }
+            StringBuffer sb=new StringBuffer();
+            Iterator<String> iterator=yearlist.iterator();
+            while (iterator.hasNext())
+            {
+                sb.append(" ").append(iterator.next());
+            }
+            out.print(sb.toString().substring(1));
+            out.close();
+        }
+        else if (type.equals("month"))
+        {
+            String year=req.getParameter("year");
+            ArrayList<String> monthlist=new ArrayList<String>();
+            for(int i=0;i<args.length;i++)
+            {
+                String[] temps=args[i].split("/");
+                String temp=temps[0]+"/"+temps[1];
+                if(!monthlist.contains(temp)&&temps[0].equals(year))
+                    monthlist.add(temp);
+            }
+            StringBuffer sb=new StringBuffer();
+            Iterator<String> iterator=monthlist.iterator();
+            while (iterator.hasNext())
+            {
+                sb.append(" ").append(iterator.next().split("/")[1]);
+            }
+            out.print(sb.toString().substring(1));
+            out.close();
+        }
+        else if(type.equals("day"))
+        {
+            String year=req.getParameter("year");
+            String month=req.getParameter("month");
+            ArrayList<String> monthlist=new ArrayList<String>();
+            for(int i=0;i<args.length;i++)
+            {
+                String[] temps=args[i].split("/");
+                if(temps[0].equals(year)&&temps[1].equals(month))
+                    monthlist.add(args[i]);
+            }
+            StringBuffer sb=new StringBuffer();
+            Iterator<String> iterator=monthlist.iterator();
+            while (iterator.hasNext())
+            {
+                sb.append(" ").append(iterator.next().split("/")[2]);
+            }
+            out.print(sb.toString().substring(1));
+            out.close();
+        }
+        System.out.println("dataselect="+dataselect);
+        System.out.println(req.getParameter("type"));
     }
 
     @Override
